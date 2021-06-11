a9c6f2e87ad8e196e53632064add73e9bf03835a
Isai Desales <isai@isai.local>
2021-06-10 23:50:30 -0500

    Test connection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core'
 import * as fileHelper from './file-helper'
-import {AuthInfo} from '@salesforce/core'
+import {AuthInfo, Connection} from '@salesforce/core'
 import * as exec from '@actions/exec'
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
   try {
@@ -14,6 +15,7 @@ async function run(): Promise<void> {
     const instanceUrl = core.getInput('instance_url')
     const decryptionKey = core.getInput('decryption_key')
     const decryptionIV = core.getInput('decryption_iv')
+
     await exec.exec('openssl', [
       'enc',
       '-nosalt',
@@ -29,15 +31,23 @@ async function run(): Promise<void> {
       '-iv',
       decryptionIV
     ])
-
-    /*const authInfo = await AuthInfo.create({
+    const privateServerKey = await fs.promises.readFile(
+      jwtKeyFile.split('/')[0].concat('/'),
+      'utf-8'
+    )
+    core.info(`Server key ${privateServerKey}`)
+    const authInfo = await AuthInfo.create({
       username: userName,
       oauth2Options: {
         loginUrl: instanceUrl,
         clientId: consumerKey,
-        privateKeyFile: jwtKeyFile
+        privateKeyFile: privateServerKey
       }
-    })*/
+    })
+    const connection = await Connection.create({authInfo})
+    const sObjectResult = await connection.describe('Account')
+
+    core.info(`Server key ${sObjectResult.name}`)
   } catch (error) {
     core.setFailed(error.message)
   }
